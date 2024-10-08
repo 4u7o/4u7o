9c970da9fdc8b687b3ca43596517821c5e7f5231
quangpao <ddquangbao@gmail.com>
2024-10-08 23:36:17 +0700

    chore 📌: Init moduleLoader for client

--- src/core/client.ts
+++ src/core/client.ts
@@ -1,7 +1,12 @@
 import { config, logger } from "4u7o";
+import type { Module } from "4u7o";
+import path from "node:path";
+import fs from "node:fs";
 import { Client, GatewayIntentBits } from "discord.js";
 
 export class _4u7oClient extends Client<boolean> {
+  private modules: Map<string, Module> = new Map();
+
   constructor() {
     super({
       intents: [
@@ -37,9 +42,38 @@ export class _4u7oClient extends Client<boolean> {
     return super.login(token);
   }
 
-  private initEvents() {
-    this.on("ready", () => {
-      logger.info("The bot has been ready!");
+  public loadModules() {
+    const moduleDir = path.resolve(__dirname, "modules");
+    const moduleFiles = fs.readdirSync(moduleDir);
+
+    moduleFiles.forEach(async (file) => {
+      const modulePath = path.join(moduleDir, file);
+      const mod: Module = (await import(modulePath)).default;
+
+      mod.load();
+      this.modules.set(mod.name, mod);
+
+      logger.info(`Loaded module: ${mod.name}`);
     });
+  }
+
+  public reloadModule(moduleName: string) {
+    const mod = this.modules.get(moduleName);
+
+    if (mod) {
+      mod.reload();
+
+      logger.info(`Reloaded module: ${mod.name}`);
+    }
+  }
+
+  public unloadModule(moduleName: string) {
+    const mod = this.modules.get(moduleName);
+
+    if (mod) {
+      mod.unload();
+
+      logger.info(`Unloaded module: ${mod.name}`);
+    }
   }
 }
