d305a138d1b1fe16182a168ebd42fc30c97e9c44
quangpao <ddquangbao@gmail.com>
2024-11-01 09:42:20 +0700

    Revert "chore: Update .env.example and package.json"
    This reverts commit f43ec8c37ea913c62eda4dbc8efe832412ced314.

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -4,15 +4,9 @@ export interface DiscordConfig {
   WEBHOOK_URL: string;
 }
 
-export interface SpotifyConfig {
-  CLIENT_ID: string;
-  CLIENT_SECRET: string;
-}
-
 export interface AppConfig {
   NODE_ENV: "development" | "production" | "test";
   discord: DiscordConfig;
-  spotify: SpotifyConfig;
 }
 
 export interface ProcessVariables extends NodeJS.ProcessEnv {
@@ -20,6 +14,4 @@ export interface ProcessVariables extends NodeJS.ProcessEnv {
   TOKEN_ID: string;
   CLIENT_ID: string;
   DISCORD_WEBHOOK_URL: string;
-  SPOTIFY_CLIENT_ID: string;
-  SPOTIFY_CLIENT_SECRET: string;
 }
